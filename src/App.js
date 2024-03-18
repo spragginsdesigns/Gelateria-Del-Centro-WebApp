4f21750d458a4cb32b375ec781fbe89e03b0f9f5
Austin Spraggins <atmosphere9999@gmail.com>
2024-03-18 15:08:19 -0700

    Add react-icons and styled-components. Added About section below hero carousel, moving onto Our Favorites section

--- src/App.js
+++ src/App.js
@@ -6,17 +6,21 @@ import Navbar from './components/navbar'; // Changed 'Navbar' to 'navbar'
 import HeroCarousel from './components/HeroCarousel'; // Added this line
 import './components/navbar.css'; // Make sure this path correctly points to your CSS file
 import './components/HeroCarousel.css'
+import About from './components/about';
+// use react.fragment instead of router;
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
-        <HeroCarousel /> {/* Added this line */}
+        <HeroCarousel />
+        <About />
         {/* Routes will go here */}
       </div>
     </Router>
   );
 }
+
 
 export default App;
---
+++ src/components/about.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './About.css';
+import logo from '../assets/Gelateria Del Centro STAMP.png'; // Import your logo
+
+const About = () => {
+    return (
+        <div>
+            <div className="about-container">
+                <div className="description">
+                    <img src={logo} alt="Store Logo" className="store-logo" />
+                    <p>Welcome to Gelateria Del Centro, your go-to destination for authentic Italian gelato in Fresno, California. Located in the bustling heart of the city, our shop is a sanctuary for gelato aficionados and dessert lovers.</p>
+                    <p>Our gelato is made fresh daily, using only the finest ingredients. We offer a wide variety of flavors, including classics like Stracciatella and Pistachio, as well as unique flavors like Lavender and Olive Oil. We also offer a selection of sorbets and vegan options.</p>
+                    {/* Rest of the description */}
+                </div>
+                <div className="sidebar">
+                    <h2>Hours</h2>
+                    <ul>
+                        <li>Open Daily - 12:00pm - 8:30 pm</li>
+                        <li>Available for Pickup and Delivery</li>
+                    </ul>
+                    <h2>Special Events</h2>
+                    <p>Check out our special events!</p>
+                    <button>Learn More</button>
+                    <h2>Our Why?</h2>
+                    <p>Our mission is to provide the best gelato experience...</p>
+                    {/* Rest of the "Our Why?" section */}
+                </div>
+            </div>
+            <hr />
+        </div>
+    );
+}
+
+export default About;
