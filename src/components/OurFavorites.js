4ee49030eee0e4f5c6d9d7bf6496fabec8470d38
Austin Spraggins <atmosphere9999@gmail.com>
2024-03-27 11:04:34 -0700

    Title: Associate Menu Items with Corresponding Images
    Details:
    1. Imported specific images for each menu item from the assets directory.
    2. Mapped each flavor category in the `favorites` constant within the `OurFavorites` component to its corresponding image.
    3. This update enhances the visual representation of each flavor category, providing a more engaging user experience.

--- src/components/OurFavorites.js
+++ src/components/OurFavorites.js
@@ -1,26 +1,48 @@
 import React from 'react';
 import './OurFavorites.css';
-import image1 from '../assets/sharon-tay-V6z3C3ru9zQ-unsplash.jpg'; // Import your images
-import image2 from '../assets/rosa-rafael-Nag3E3yzygU-unsplash.jpg';
-import image3 from '../assets/alana-harris-g8gB5FKFAP4-unsplash.jpg';
-import image4 from '../assets/kyle-hinkson-H75RvPCvkik-unsplash.jpg';
+import image1 from '../assets/menu-tems/web/Cashew_Milk_Himalayan_Salted_Caramel-web.jpg'; // Import your images
+import image2 from '../assets/menu-tems/web/Affogato-web.jpg';
+import image3 from '../assets/menu-tems/web/Caramello_Chocolate_Crunch-web.jpg';
+import image4 from '../assets/menu-tems/web/Chocolate_Strawberry-web.jpg';
+import image5 from '../assets/menu-tems/web/Coconut_Milk_Indonesian_Vanilla_Bean-web.jpg';
+import image7 from '../assets/menu-tems/web/Dark_Chocolate-web.jpg';
+import image8 from '../assets/menu-tems/web/Honey_Lavender-web.jpg';
+import image9 from '../assets/menu-tems/web/Limoncello-web.jpg';
+import image11 from '../assets/menu-tems/web/Pistachio-web.jpg';
+import image12 from '../assets/menu-tems/web/Stracchiatella-web.jpg';
+import image13 from '../assets/menu-tems/web/Crème_Brûlée-web.jpg';
+
+
 // Import more images as needed
 
 const OurFavorites = () => {
     const favorites = [
-        { flavor: 'PB Cashew Milk Himalayan Salted Caramel', name: 'Vegan', description: 'A savory-sweet fusion of peanut butter and salted caramel, balanced with the nuttiness of cashew milk.', image: image3 },
-        { flavor: 'PB Coconut Milk Indonesian Vanilla Bean', name: 'Vegan', description: 'An exotic escape featuring the depth of Indonesian vanilla and dark peanut butter, lightened by coconut milk.', image: image4 },
-        { flavor: 'PB Strawberry Almond Crisp', name: 'Vegan', description: 'A fruity concoction of strawberries and almond crisp, perfected with a swirl of peanut butter.', image: image1 },
-        { flavor: 'Stracciatella', name: 'Single, Double, or Triple Scoop', description: 'Italian-inspired gelato with slivers of chocolate, creating a delicate yet rich textural delight.', image: image2 },
-        { flavor: 'Dark Chocolate', name: 'Single, Double, or Triple Scoop', description: 'Indulge in the deep, rich flavors of dark chocolate - a timeless classic for the discerning palate.', image: image3 },
-        { flavor: 'Pistachio', name: 'Single, Double, or Triple Scoop', description: 'A silky-smooth gelato infused with the finest pistachios for a distinctly nutty and luxurious taste.', image: image4 },
-        { flavor: 'Limoncello', name: 'Single, Double, or Triple Scoop', description: 'Zesty limoncello liqueur meets creamy gelato for a refreshing and spirited Italian treat.', image: image1 },
-        { flavor: 'Caramello Chocolate Crunch', name: 'Single, Double, or Triple Scoop', description: 'The ultimate treat combining gooey caramel and crunchy chocolate pieces in a creamy base.', image: image2 },
-        { flavor: 'Honey Lavender', name: 'Single, Double, or Triple Scoop', description: 'Floral notes of lavender paired with sweet honey create a serene and sophisticated flavor profile.', image: image3 },
-        { flavor: 'Bavarian Mint', name: 'Single, Double, or Triple Scoop', description: 'A cool breath of mint infused into a rich and creamy Bavarian-style gelato.', image: image4 },
-        { flavor: 'Oatmilk Strawberry Almond Crisp', name: 'Single, Double, or Triple Scoop', description: 'Vegan delight with the sweetness of strawberries and the crunch of almonds, all in creamy oat milk gelato.', image: image1 },
+        { flavor: 'PB Cashew Milk Himalayan Salted Caramel', name: 'Vegan', description: 'A savory-sweet fusion of peanut butter and salted caramel, balanced with the nuttiness of cashew milk.', image: image1 },
+
+        { flavor: 'PB Coconut Milk Indonesian Vanilla Bean', name: 'Vegan', description: 'An exotic escape featuring the depth of Indonesian vanilla and dark peanut butter, lightened by coconut milk.', image: image5 },
+
+        { flavor: 'PB Strawberry Almond Crisp', name: 'Vegan', description: 'A fruity concoction of strawberries and almond crisp, perfected with a swirl of peanut butter.', image: image4 },
+
+        { flavor: 'Stracciatella', name: 'Single, Double, or Triple Scoop', description: 'Italian-inspired gelato with slivers of chocolate, creating a delicate yet rich textural delight.', image: image12 },
+
+        { flavor: 'Chocolate Strawberry', name: 'Single, Double, or Triple Scoop', description: 'A classic pairing of rich chocolate and ripe strawberries, blended into a creamy and indulgent treat.', image: image4 },
+
+        { flavor: 'Dark Chocolate', name: 'Single, Double, or Triple Scoop', description: 'Indulge in the deep, rich flavors of dark chocolate - a timeless classic for the discerning palate.', image: image7 },
+
+        { flavor: 'Pistachio', name: 'Single, Double, or Triple Scoop', description: 'A silky-smooth gelato infused with the finest pistachios for a distinctly nutty and luxurious taste.', image: image11 },
+
+        { flavor: 'Limoncello', name: 'Single, Double, or Triple Scoop', description: 'Zesty limoncello liqueur meets creamy gelato for a refreshing and spirited Italian treat.', image: image9 },
+
+        { flavor: 'Caramello Chocolate Crunch', name: 'Single, Double, or Triple Scoop', description: 'The ultimate treat combining gooey caramel and crunchy chocolate pieces in a creamy base.', image: image3 },
+
+        { flavor: 'Honey Lavender', name: 'Single, Double, or Triple Scoop', description: 'Floral notes of lavender paired with sweet honey create a serene and sophisticated flavor profile.', image: image8 },
+
+        // Crème_Brûlée flavor
+        { flavor: 'Crème Brûlée', name: 'Single, Double, or Triple Scoop', description: 'A decadent dessert in gelato form, featuring a rich custard base and a caramelized sugar topping.', image: image13 },
+
+        { flavor: 'Bavarian Mint', name: 'Single, Double, or Triple Scoop', description: 'A cool breath of mint infused into a rich and creamy Bavarian-style gelato.', image: image1 },
+
         { flavor: 'Nonie’s Spumoni', name: 'Single, Double, or Triple Scoop', description: 'A tribute to tradition, this spumoni twists cherry, pistachio, and chocolate into a symphony of flavor.', image: image2 },
-        { flavor: 'Coconut Milk Vanilla Bean', name: 'Vegan', description: 'An indulgent blend of tropical coconut milk and rich vanilla beans for a smooth and exotic treat.', image: image3 },
     ]
 
 
